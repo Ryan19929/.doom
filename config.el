b56f5ed68e99959e0c5e7f6e4cddf0b9e39a3cd4
ryan <921581278@qq.com>
2022-04-13 22:15:34 +0800

    remove

--- config.el
+++ config.el
@@ -16,20 +16,6 @@
         rime-predicate-prog-in-code-p))
 ;; 针对 rime posframe 最后一项丢失的bug
 
-(defun +rime--posframe-display-content-a (args)
-"给 `rime--posframe-display-content' 传入的字符串加一个全角空
-格，以解决 `posframe' 偶尔吃字的问题。"
-(cl-destructuring-bind (content) args
-(let ((newresult (if (string-blank-p content)
-                        content
-                        (concat content "　"))))
-(list newresult))))
-
-(if (fboundp 'rime--posframe-display-content)
-(advice-add 'rime--posframe-display-content
-                :filter-args
-                #'+rime--posframe-display-content-a)
-(error "Function `rime--posframe-display-content' is not available."))
 
 (set-language-environment "utf-8")
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))
